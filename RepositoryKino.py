dda4aeca52502a2813996eebfc318e63d4b60bf9
LukaszM92 <lmicalek@gmail.com>
2017-02-04 11:10:45 +0100

    Update RepositoryKino.py

--- RepositoryKino.py
+++ RepositoryKino.py
@@ -23,33 +23,33 @@ class RepositoryException(Exception):
 class Filmy():
     """
     """
-    def __init__(self, id, tytul=(), gatunek=(), dlugosc=(), Seanse=[]):
+    def __init__(self, id, tytul=(), gatunek=(), dlugosc=(), seanse=[]):
         self.id = id
         self.tytul = tytul
         self.gatunek = gatunek
         self.dlugosc = dlugosc
-        self.Seanse = Seanse
+        self.seanse = seanse
 
 
 
     def __repr__(self):
         return "<Filmy(id='%s', tytul='%s', items='%s')>" % (
-                    self.id, self.tytul, self.Seanse
+                    self.id, self.tytul, self.seanse
                 )
 
 
 class SeanseItem():
     """Model występuje tylko wewnątrz obiektu Filmy.
     """
-    def __init__(self, Sala, Dzien, Godzina):
-        self.Sala = Sala
-        self.Dzien = Dzien
-        self.Godzina = Godzina
+    def __init__(self, sala, dzien, godzina):
+        self.sala = sala
+        self.dzien = dzien
+        self.godzina = godzina
 
 
     def __repr__(self):
-        return "<SeanseItem(Sala='%s', Dzien='%s', Godzina='%s')>" % (
-                    self.Sala, self.Dzien, self.Godzina
+        return "<SeanseItem(sala='%s', dzien='%s', godzina='%s')>" % (
+                    self.sala, self.dzien, self.godzina
                 )
 
 
@@ -98,7 +98,7 @@ class Repository():
 #
 class FilmyRepository(Repository):
 
-    def add(self, Filmy):
+    def add(self, filmy):
         """Metoda dodaje pojedynczy film do bazy danych,
         wraz ze wszystkimi jego pozycjami
         """
@@ -106,164 +106,164 @@ class FilmyRepository(Repository):
             c = self.conn.cursor()
 
             c.execute('INSERT INTO Filmy (id, tytul, gatunek, dlugosc) VALUES(?, ?, ?, ?)',
-                        (Filmy.id, Filmy.tytul, Filmy.gatunek, filmy.dlugosc)
+                        (filmy.id, filmy.tytul, filmy.gatunek, filmy.dlugosc)
                     )
             # zapisz pozycje faktury
-            if filmy.Seanse:
-                for Seanseitem in filmy.Seanse:
+            if filmy.seanse:
+                for seanseitem in filmy.seanse:
                     try:
-                        c.execute('INSERT INTO Seanse (Sala, Dzien, Godzina, Filmy_id) VALUES(?,?,?,?)',
-                                        (Seanseitem.Sala, Seanseitem.Dzien, Seanseitem.Godzina, Filmy_id)
+                        c.execute('INSERT INTO Seanse (sala, dzien, godzina, filmy_id) VALUES(?,?,?,?)',
+                                        (seanseitem.sala, seanseitem.dzien, seanseitem.godzina, filmy.id)
                                 )
                     except Exception as e:
                         #print "item add error:", e
-                        raise RepositoryException('error adding Filmy item: %s, to Filmy: %s' %
-                                                    (str(Seanseitem), str(Filmy.id))
+                        raise RepositoryException('error adding filmy item: %s, to filmy: %s' %
+                                                    (str(seanseitem), str(filmy.id))
                                                 )
         except Exception as e:
             #print "Filmy add error:", e
-            raise RepositoryException('error adding Filmy %s' % str(Filmy))
+            raise RepositoryException('error adding filmy %s' % str(filmy))
 
-    def delete(self, Filmy):
+    def delete(self, filmy):
 
         try:
             c = self.conn.cursor()
             # usuń pozycje
-            c.execute('DELETE FROM Seanse WHERE Filmy_id=?', (Filmy.id,))
+            c.execute('DELETE FROM Seanse WHERE filmy_id=?', (filmy.id,))
             # usuń nagłowek
-            c.execute('DELETE FROM Filmy WHERE id=?', (Filmy.id,))
+            c.execute('DELETE FROM Filmy WHERE id=?', (filmy.id,))
 
         except Exception as e:
             #print "Filmy delete error:", e
-            raise RepositoryException('error deleting Filmy %s' % str(Filmy))
+            raise RepositoryException('error deleting filmy %s' % str(filmy))
 
     def getById(self, id):
-        """Get Filmy by id
+        """Get filmy by id
         """
         try:
             c = self.conn.cursor()
             c.execute("SELECT * FROM Filmy WHERE id=?", (id,))
-            Seanse_row = c.fetchone()
-            Filmy = Filmy(id=id)
-            if Seanse_row == None:
-                Filmy=None
+            seanse_row = c.fetchone()
+            filmy = Filmy(id=id)
+            if seanse_row == None:
+                filmy=None
             else:
-                Filmy.Sala = Seanse_row[1]
-                c.execute("SELECT * FROM Seanse WHERE Filmy_id=? order by Dzien", (id,))
-                Seanse_items_rows = c.fetchall()
+                filmy.sala = seanse_row[1]
+                c.execute("SELECT * FROM Seanse WHERE filmy_id=? order by dzien", (id,))
+                seanse_items_rows = c.fetchall()
                 items_list = []
-                for item_row in Seanse_items_rows:
-                    item = SeanseItem(Sala=item_row[0], Dzien=item_row[1], Godzina=item_row[2])
+                for item_row in seanse_items_rows:
+                    item = SeanseItem(sala=item_row[0], dzien=item_row[1], godzina=item_row[2])
                     items_list.append(item)
-                Filmy.Seanse=items_list
+                filmy.seanse=items_list
         except Exception as e:
             #print "Filmy getById error:", e
-            raise RepositoryException('error getting by id Filmy_id: %s' % str(id))
-        return Filmy
+            raise RepositoryException('error getting by id filmy_id: %s' % str(id))
+        return filmy
 
-    def update(self, Filmy):
+    def update(self, filmy):
         """Metoda uaktualnia Filmy w bazie danych,
         wraz ze wszystkimi pozycjami.
         """
         try:
             # pobierz z bazy Filmy
-            Seanse_oryg = self.getById(Filmy.id)
-            if Seanse_oryg != None:
+            seanse_oryg = self.getById(filmy.id)
+            if seanse_oryg != None:
                 # Filmy jest w bazie: usuń ją
-                self.delete(Filmy)
-            self.add(Filmy)
+                self.delete(filmy)
+            self.add(filmy)
 
         except Exception as e:
             #print "Filmy update error:", e
-            raise RepositoryException('error updating Filmy %s' % str(Filmy))
+            raise RepositoryException('error updating filmy %s' % str(filmy))
 
 
 
 if __name__ == '__main__':
     try:
-        with FilmyRepository() as Filmy_repository:
-            Filmy_repository.add(
-                Filmy(id = 1, tytul='Szeregowiec Ryan', gatunek='Wojenne',
-                        Seanse = [
-                            SeanseItem(Sala = "Alfa",   Dzien = '2017-02-16', Godzina = '16:00:00'),
-                            SeanseItem(Sala = "Beta",   Dzien = '2017-02-16', Godzina = '20:00:00'),
-                            SeanseItem(Sala = "Gamma",  Dzien = '2017-02-17', Godzina = '16:00:00'),
-                            SeanseItem(Sala = "Delta",  Dzien = '2017-02-17', Godzina = '20:00:00'),
-                            SeanseItem(Sala = "Omega",  Dzien = '2017-02-17', Godzina = '22:00:00')
+        with FilmyRepository() as filmy_repository:
+            filmy_repository.add(
+                Filmy(id = 1, tytul='Szeregowiec Ryan', gatunek='Wojenne', dlugosc=1,
+                        seanse = [
+                            SeanseItem(sala = "Alfa",   dzien = '2017-02-16', godzina = '16:00:00'),
+                            SeanseItem(sala = "Beta",   dzien = '2017-02-16', godzina = '20:00:00'),
+                            SeanseItem(sala = "Gamma",  dzien = '2017-02-17', godzina = '16:00:00'),
+                            SeanseItem(sala = "Delta",  dzien = '2017-02-17', godzina = '20:00:00'),
+                            SeanseItem(sala = "Omega",  dzien = '2017-02-17', godzina = '22:00:00')
                         ]
                     )
                 )
-            Filmy_repository.complete()
+            filmy_repository.complete()
     except RepositoryException as e:
         print(e)
 
     print FilmyRepository().getById(1)
-
-if __name__ == '__main__':
-    try:
-        with FilmyRepository() as Filmy_repository:
-            Filmy_repository.add(
-                Filmy(id = 2, tytul='Titanic', gatunek='Melodramat',
-                        Seanse = [
-                            SeanseItem(Sala = "Alfa",   Dzien = '2017-02-18', Godzina = '15:00:00'),
-                            SeanseItem(Sala = "Beta",   Dzien = '2017-02-18', Godzina = '20:00:00'),
-                            SeanseItem(Sala = "Gamma",  Dzien = '2017-02-19', Godzina = '15:00:00'),
-                            SeanseItem(Sala = "Delta",  Dzien = '2017-02-19', Godzina = '20:00:00'),
-                            SeanseItem(Sala = "Omega",  Dzien = '2017-02-19', Godzina = '21:00:00')
-                        ]
-                    )
-                )
-            Filmy_repository.complete()
-    except RepositoryException as e:
-        print(e)
-
-    print FilmyRepository().getById(2)
-
-if __name__ == '__main__':
-    try:
-        with FilmyRepository() as Filmy_repository:
-            Filmy_repository.add(
-                Filmy(id = 3, tytul='Terminator', gatunek='Akcja',
-                        Seanse = [
-                            SeanseItem(Sala = "Alfa",   Dzien = '2017-02-20', Godzina = '17:00:00'),
-                            SeanseItem(Sala = "Beta",   Dzien = '2017-02-20', Godzina = '20:00:00'),
-                            SeanseItem(Sala = "Gamma",  Dzien = '2017-02-21', Godzina = '17:00:00'),
-                            SeanseItem(Sala = "Delta",  Dzien = '2017-02-21', Godzina = '20:00:00'),
-                            SeanseItem(Sala = "Omega",  Dzien = '2017-02-21', Godzina = '22:00:00')
-                        ]
-                    )
-                )
-            Filmy_repository.complete()
-    except RepositoryException as e:
-        print(e)
-
-    print FilmyRepository().getById(3)
-
-
-    try:
-        with FilmyRepository() as FilmyRepository_repository:
-            Filmy_repository.update(
-                Filmy(id = 1, tytul='Szeregowiec Ryan', gatunek='Wojenne',
-                        Seanse = [
-                            SeanseItem(Sala = "Alfa",   Dzien = '2017-02-16', Godzina = '16:30:00'),
-                            SeanseItem(Sala = "Beta",   Dzien = '2017-02-16', Godzina = '21:00:00'),
-                            SeanseItem(Sala = "Gamma",   Dzien = '2017-02-17', Godzina = '16:00:00'),
-                            SeanseItem(Sala = "Delta",   Dzien = '2017-02-17', Godzina = '20:00:00'),
-                            SeanseItem(Sala = "Omega",   nDzien = '2017-02-18', Godzina = '22:00:00')
-                        ]
-                    )
-                )
-            Filmy_repository.complete()
-    except RepositoryException as e:
-        print(e)
-
-    print FilmyRepository().getById(1)
-
-
-
-    try:
-        with FilmyRepository() as Filmy_repository:
-            Filmy_repository.delete( Filmy(id = 2) )
-            Filmy_repository.complete()
-    except RepositoryException as e:
-        print(e)
+#
+# if __name__ == '__main__':
+#     try:
+#         with FilmyRepository() as Filmy_repository:
+#             Filmy_repository.add(
+#                 Filmy(id = 2, tytul='Titanic', gatunek='Melodramat',
+#                         Seanse = [
+#                             SeanseItem(Sala = "Alfa",   Dzien = '2017-02-18', Godzina = '15:00:00'),
+#                             SeanseItem(Sala = "Beta",   Dzien = '2017-02-18', Godzina = '20:00:00'),
+#                             SeanseItem(Sala = "Gamma",  Dzien = '2017-02-19', Godzina = '15:00:00'),
+#                             SeanseItem(Sala = "Delta",  Dzien = '2017-02-19', Godzina = '20:00:00'),
+#                             SeanseItem(Sala = "Omega",  Dzien = '2017-02-19', Godzina = '21:00:00')
+#                         ]
+#                     )
+#                 )
+#             Filmy_repository.complete()
+#     except RepositoryException as e:
+#         print(e)
+#
+#     print FilmyRepository().getById(2)
+#
+# if __name__ == '__main__':
+#     try:
+#         with FilmyRepository() as Filmy_repository:
+#             Filmy_repository.add(
+#                 Filmy(id = 3, tytul='Terminator', gatunek='Akcja',
+#                         Seanse = [
+#                             SeanseItem(Sala = "Alfa",   Dzien = '2017-02-20', Godzina = '17:00:00'),
+#                             SeanseItem(Sala = "Beta",   Dzien = '2017-02-20', Godzina = '20:00:00'),
+#                             SeanseItem(Sala = "Gamma",  Dzien = '2017-02-21', Godzina = '17:00:00'),
+#                             SeanseItem(Sala = "Delta",  Dzien = '2017-02-21', Godzina = '20:00:00'),
+#                             SeanseItem(Sala = "Omega",  Dzien = '2017-02-21', Godzina = '22:00:00')
+#                         ]
+#                     )
+#                 )
+#             Filmy_repository.complete()
+#     except RepositoryException as e:
+#         print(e)
+#
+#     print FilmyRepository().getById(3)
+#
+#
+#     try:
+#         with FilmyRepository() as FilmyRepository_repository:
+#             Filmy_repository.update(
+#                 Filmy(id = 1, tytul='Szeregowiec Ryan', gatunek='Wojenne',
+#                         Seanse = [
+#                             SeanseItem(Sala = "Alfa",   Dzien = '2017-02-16', Godzina = '16:30:00'),
+#                             SeanseItem(Sala = "Beta",   Dzien = '2017-02-16', Godzina = '21:00:00'),
+#                             SeanseItem(Sala = "Gamma",   Dzien = '2017-02-17', Godzina = '16:00:00'),
+#                             SeanseItem(Sala = "Delta",   Dzien = '2017-02-17', Godzina = '20:00:00'),
+#                             SeanseItem(Sala = "Omega",   nDzien = '2017-02-18', Godzina = '22:00:00')
+#                         ]
+#                     )
+#                 )
+#             Filmy_repository.complete()
+#     except RepositoryException as e:
+#         print(e)
+#
+#     print FilmyRepository().getById(1)
+#
+#
+#
+#     try:
+#         with FilmyRepository() as Filmy_repository:
+#             Filmy_repository.delete( Filmy(id = 2) )
+#             Filmy_repository.complete()
+#     except RepositoryException as e:
+#         print(e)
